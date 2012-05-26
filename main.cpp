b73842c4d8d9475912522c59f0cc69dc185ecb16
Roger Lam <mrlamroger@gmail.com>
2012-05-26 00:53:50 -0700

    More testing
    Github for Windows works well!

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main() {
   Zerg player1;
-  cout << player1.minerals_per_min()  << "\n";
+  cout << "Player 1 Minerals per Minute: " << player1.minerals_per_min()  << "\n";
+  cout << "Player 1 Gas per Minute: " << player1.gas_per_min()  << "\n";
   return 1;
 }
