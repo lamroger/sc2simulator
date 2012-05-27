4af2906f08ee0862211d3fc40ce002323358e4bf
Roger Lam <mrlamroger@gmail.com>
2012-05-27 04:24:37 -0700

    added print function and making unit function

--- zerg.cpp
+++ zerg.cpp
@@ -1,6 +1,9 @@
 #include "zerg.h"
 
 Zerg::Zerg() {
+  unit_count[0] = 6; //Initial Drones
+  unit_count[1] = 1; //Initial Overlord
+  
   mineral_workers = 6;
   gas_workers = 0;
   mining_bases = 1;
@@ -10,6 +13,8 @@ Zerg::Zerg() {
   
   minerals = 50;
   gas = 0;
+
+  
 }
 
 int Zerg::minerals_per_min() {
@@ -18,4 +23,144 @@ int Zerg::minerals_per_min() {
 
 int Zerg::gas_per_min() {
   return gas_workers*38;
+}
+
+void Zerg::print() {
+	cout << "Minerals per Minute: " << minerals_per_min()  << "\n";
+	cout << "Gas per Minute: " << gas_per_min()  << "\n";
+	cout << "\n";
+	cout << "Mineral Workers: " << mineral_workers  << "\n";
+	cout << "Gas Workers: " << gas_workers  << "\n";
+	cout << "Mining Bases: " << mining_bases  << "\n";
+	cout << "Max Supply: " << max_supply  << "\n";
+	cout << "Army Value: " << army_value  << "\n";
+	cout << "Larva: " << larva  << "\n";
+}
+
+void Zerg::make_unit(int index) {
+	switch (index) {
+		case 0:
+			if (larva > 0) {
+				larva--;
+				unit_count[index]++;
+				mineral_workers++;
+			} else {
+				//Error: not enough larva
+			}
+			break;
+
+		case 1:
+			if (larva > 0) {
+				larva--;
+				unit_count[index]++;
+				max_supply += 8;
+			} else {
+				//Error: not enough larva
+			}
+			break;
+
+		case 2:
+			if (larva > 0) {
+				larva--;
+				unit_count[index]++;
+				army_value += 1;
+			} else {
+				//Error: not enough larva
+			}
+			break;
+
+		case 3:
+			
+			//account for # hatches and build time
+			unit_count[index]++;
+			army_value += 2;
+			break;
+
+		case 4:
+			//doesnt do much since we dont accout for cloaking/scouting
+			break;
+
+		case 5:
+			if (larva > 0) {
+				larva--;
+				unit_count[index]++;
+				army_value += 2;
+			} else {
+				//Error: not enough larva
+			}
+			break;
+
+		case 6:
+			// each supply of zerglings give 2 lings, baneling takes only one ling but we assume it take 1 supply instead
+			if (unit_count[2] > 0) {
+				unit_count[2]--;
+				unit_count[index]++;
+			} else {
+				//Error: not enough larva
+			}
+			break;
+
+		case 7:
+			if (larva > 0) {
+				larva--;
+				unit_count[index]++;
+				army_value += 2;
+			} else {
+				//Error: not enough larva
+			}
+			break;
+
+		case 8:
+			if (larva > 0) {
+				larva--;
+				unit_count[index]++;
+				army_value += 2;
+			} else {
+				//Error: not enough larva
+			}
+			break;
+
+		case 9:
+			if (larva > 0) {
+				larva--;
+				unit_count[index]++;
+				army_value += 2;
+			} else {
+				//Error: not enough larva
+			}
+			break;
+
+		case 10:
+			if (larva > 0) {
+				larva--;
+				unit_count[index]++;
+				army_value += 2;
+			} else {
+				//Error: not enough larva
+			}
+			break;
+
+		case 11:
+			if (larva > 0) {
+				larva--;
+				unit_count[index]++;
+				army_value += 6; //WHOPPING 6 SUPPLY!?!?!
+			} else {
+				//Error: not enough larva
+			}
+			break;
+
+		case 12:
+			if (unit_count[10] > 0) {
+				unit_count[10];
+				unit_count[index]++;
+				army_value += 2; // total of 4 supply (corruptor + morphing)
+			} else {
+				//Error: not enough larva
+			}
+			break;
+		default:
+			// Error: index out of range, bad input
+			break;
+	}
 }
--- zerg.h
+++ zerg.h
@@ -1,55 +1,67 @@
-class Zerg {
+#include "race.h"
 
-  // Units
-  class drone;
-  class overlord;
-  class zergling;
-  class queen;
-  class overseer;
-  class roach;
-  class baneling;
-  class hydralisk;
-  class infestor;
-  class mutalisk;
-  class corruptor;
-  class ultralisk;
-  class broodlord;
+class Zerg: public Race {
+
+	/*
+	Not implementing Nested classes yet. 
+	Trying to get basic model working. 
+	*/
+	
+	// Units
+	class drone; // Index 0
+	class overlord; // Index 1
+	class zergling; // Index 2
+	class queen; // Index 3
+	class overseer; // Index 4
+	class roach; // Index 5
+	class baneling; // Index 6
+	class hydralisk; // Index 7
+	class infestor; // Index 8
+	class mutalisk; // Index 9
+	class corruptor; // Index 10
+	class ultralisk; // Index 11
+	class broodlord; // Index 12
   
-  // Buildings
-  //class extractor;
-  class hatchery;
-  class evolution_chamber;
-  class spawning_pool;
-  class roach_warren;
-  class baneling_nest;
-  class lair;
-  class hydralisk_den;
-  class infestation_pit;
-  class spire;
-  class nydus_network;
-  class hive;
-  class ultralisk_cavern;
-  class greater_spire;
+	// Buildings
+	//class extractor;
+	class hatchery;
+	class evolution_chamber;
+	class spawning_pool;
+	class roach_warren;
+	class baneling_nest;
+	class lair;
+	class hydralisk_den;
+	class infestation_pit;
+	class spire;
+	class nydus_network;
+	class hive;
+	class ultralisk_cavern;
+	class greater_spire;
   
-  // Defensive Structures
-  // calculate when defending?
-  //class spore_crawler;
-  //class spine_crawler;
+	// Defensive Structures
+	// calculate when defending?
+	//class spore_crawler;
+	//class spine_crawler;
   
 public:
-  Zerg();
-  int minerals_per_min();
-  int gas_per_min();
+	Zerg();
+	int minerals_per_min();
+	int gas_per_min();
+	void print();
+	void make_unit(int index);
   
 private:
-  int mineral_workers;
-  int gas_workers;
-  int mining_bases;
-  int army_value;
-  int larva;
-  int max_supply;
+	int mineral_workers;
+	int gas_workers;
+	int mining_bases;
+	int army_value;
+	int larva;
+	int max_supply;
   
-  int minerals;
-  int gas;
+	int minerals;
+	int gas;
   
+	// 13 different units (You guys should watch 13 Assassins.) 
+	int unit_count[13];
+
 };
